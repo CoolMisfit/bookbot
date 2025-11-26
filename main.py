fc6fbe6ceed0d86ca8a992544489f4444d1ab5e9
CoolMisfit <talal@bayaa.co.uk>
2025-11-26 22:24:58 +0200

    implemented sys.argv

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import sys
 from stats import get_num_words
 from stats import get_num_chars
 from stats import get_sorted
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
 
@@ -12,7 +16,7 @@ def main():
     # print(num_chars)
     sorted_dict_list = get_sorted(num_chars)
     print("============ BOOKBOT ============")
-    print("Analysing book found at books/frankenstein.txt...")
+    print(f"Analysing book found at {sys.argv[1]}...")
     print("----------- Word Count -----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count ---------")
